0464c0241efea07353b2e5781cf0ed1b46784ecd
Beto Toro <betotoro0902@gmail.com>
2022-09-19 10:56:31 -0500

    [feat]: first layout of the project

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      NotificationsView()
+  var body: some View {
+    TabView {
+      ProspectsView(filter: .none)
+        .tabItem {
+          Label("Everyone", systemImage: "person.3")
+        }
+      ProspectsView(filter: .contacted)
+        .tabItem {
+          Label("Contacted", systemImage: "checkmark.circle")
+        }
+      ProspectsView(filter: .uncontacted)
+        .tabItem {
+          Label("Uncontacted", systemImage: "questionmark.diamond")
+        }
+      MeView()
+        .tabItem {
+          Label("Me", systemImage: "person.crop.square")
+        }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
