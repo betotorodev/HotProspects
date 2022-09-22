33eff8aa73b390e24539a0b6b1e48f252643b735
Beto Toro <betotoro0902@gmail.com>
2022-09-22 11:03:23 -0500

    [feat]: finish project

--- HotProspects/views/MeView.swift
+++ HotProspects/views/MeView.swift
@@ -12,9 +12,14 @@ import SwiftUI
 struct MeView: View {
   @State private var name = "Anonymous"
   @State private var emailAddress = "you@yoursite.com"
+  @State private var qrCode = UIImage()
   
   let context = CIContext()
   let filter = CIFilter.qrCodeGenerator()
+  
+  func updateCode() {
+    qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+  }
   
   func generateQRCode(from string: String) -> UIImage {
     filter.message = Data(string.utf8)
@@ -41,15 +46,26 @@ struct MeView: View {
         
         HStack {
           Spacer()
-          Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+          Image(uiImage: qrCode)
             .resizable()
             .interpolation(.none)
             .scaledToFit()
             .frame(width: 200, height: 200)
+            .contextMenu {
+              Button {
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: qrCode)
+              } label: {
+                Label("Save to Photos", systemImage: "square.and.arrow.down")
+              }
+            }
           Spacer()
         }
       }
       .navigationTitle("Your code")
+      .onAppear(perform: updateCode)
+      .onChange(of: name) { _ in updateCode() }
+      .onChange(of: emailAddress) { _ in updateCode() }
     }
   }
 }
